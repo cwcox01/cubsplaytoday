f664f945d42293e9f5ffb51684b5ff0b993f6e2a
Cory Cox <corycox@Corys-MBP.lan>
2023-04-02 14:14:43 -0400

    click outside nav on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 // importing NavbarDisplay for navbar into smaller component
 import NavbarDisplay from "./NavbarDisplay";
 
 function Navbar() {
   const [isActive, setIsActive] = useState(false);
+  const navbarRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+        setIsActive(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [navbarRef]);
+
   return (
-    <div className="navbar">
+    <div className="navbar" ref={navbarRef}>
       <h1 className="site-header ps-3">Cubs</h1>
       <div className="cubs-flag-nav cubs-flag-nac ps-3 pe-3">
         <h1 className="mb-0">W</h1>
